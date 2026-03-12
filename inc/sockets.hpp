4fa2ac0b8017153eceffe60d12a9f07c4aff943c
gzovkic <g.zovkic@web.de>
2026-03-12 09:21:24 +0100

    ADD(src/sockets/sockets.cpp, inc/sockets.hpp): implement socket setup functionality with error handling

--- inc/sockets.hpp
+++ inc/sockets.hpp
@@ -2,4 +2,14 @@
 
 #include "webserv.hpp"
 
+#include <sys/types.h>
+#include <sys/socket.h>    
+#include <netinet/in.h>    
+#include <arpa/inet.h>     
+#include <fcntl.h>         
+#include <unistd.h>        
+#include <cstring>         
+#include <cerrno>
+#include <netdb.h>
+
 void setup_sockets(void);
--- src/sockets/sockets.cpp
+++ src/sockets/sockets.cpp
@@ -2,5 +2,32 @@
 
 void setup_sockets(void)
 {
-    std::cout << YELLOW << "sockets here" << RESET << std::endl;
+    int socket_fd;
+    struct addrinfo hints;
+
+    memset(&hints, 0 , sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    int status;
+    std::string port("8080");
+
+    struct addrinfo *results;
+    status = getaddrinfo(NULL, port.c_str(), &hints, &results);
+    if(status != 0)
+    {
+        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        return ;
+    }
+
+    socket_fd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+    if(socket_fd == -1)
+    {
+        std::cerr << "socket error: " << strerror(errno) << std::endl;
+        freeaddrinfo(results);
+        return ;
+    }
+
+    freeaddrinfo(results);
 }
