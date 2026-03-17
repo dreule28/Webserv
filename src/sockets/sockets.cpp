003182e38d16967287fd13b25bef498fad140290
gzovkic <g.zovkic@web.de>
2026-03-17 14:42:28 +0100

    REFINE(src/sockets/sockets.cpp): enhance client socket handling with error logging and request processing

--- src/sockets/sockets.cpp
+++ src/sockets/sockets.cpp
@@ -8,13 +8,57 @@ void print_poll_fds(std::vector<Socket> &poll_fds)
     }
 }
 
+Socket create_client_socket(Socket fds)
+{
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    
+    int client_fd = accept(fds._poll_fd.fd, 
+                 (struct sockaddr *)&client_addr, 
+                  &client_addr_len);
+    if(client_fd == -1){
+        std::cerr << RED << "accept error" << RESET << std::endl;
+    } else {
+        std::cout << GREEN << "Client connected: " << client_fd << RESET << std::endl;
+    }
+    
+    if (fcntl(client_fd, F_SETFL, O_NONBLOCK) == -1)
+    {
+        std::cerr << RED << "fcntl error: " << strerror(errno) << RESET << std::endl;
+        exit(-1);
+    }
+// Create new Socket for the client
+    Socket client_socket;
+    client_socket._poll_fd.fd = client_fd;
+    client_socket._poll_fd.events = POLLIN;
+    client_socket._poll_fd.revents = 0;
+    client_socket._fd_flag = CLIENT_FD;
+    
+    return(client_socket);
+}
+
+void handleRequest(Socket fd)
+{
+    char buffer[200];
+    ssize_t bytes = recv(fd._poll_fd.fd, buffer, sizeof(buffer), 0);
+    if(bytes == 0){
+        std::cout << YELLOW << "Client disconnected" << RESET << std::endl;
+        return ;
+    } else if (bytes == -1){
+        std::cerr << RED << "recv error: " << strerror(errno) << RESET << std::endl;
+        return ;
+    }
+
+    buffer[bytes] = '\0';
+    fd._read_buffer.append(buffer, bytes);
+    std::cout << BOLD << BLUE << fd._read_buffer << RESET << std::endl;
+}
+
 void event_loop(std::vector<Socket> fds){
 	
 	while (true){
-		bool break_flag = false;
 
 		std::vector<struct pollfd> poll_array(fds.size());
-        
         for (size_t i = 0; i < fds.size(); i++){
             poll_array[i] = fds[i]._poll_fd;
         }
@@ -27,14 +71,25 @@ void event_loop(std::vector<Socket> fds){
         for(size_t i = 0; i < fds.size(); i++){
             if(poll_array[i].revents & POLLIN){
                 std::cout << BLUE << "POLLIN" << RESET << std::endl;
-                break_flag = true;
+                if(fds[i]._fd_flag == SERVER_FD){
+                    std::cout << BLUE << "SERVER_FD route" << RESET << std::endl;
+                        Socket client_socket;
+
+                        client_socket = create_client_socket(fds[i]);
+                        client_socket._index = fds.size();
+                        fds.push_back(client_socket);
+                    } else if(fds[i]._fd_flag == CLIENT_FD){
+                    std::cout << BLUE << "CLIENT_FD route" << RESET << std::endl;
+                    handleRequest(fds[i]);
+                }
+        
             }
         }
-		if(break_flag)
-			break;
-	}
+        }
 
 }
+
+
 
 void setup_sockets(void)
 {
