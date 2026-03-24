412afb44051daca072dc0fa47f6c351d5b5f72bf
Anakin Pregitzer <anakin@pregitzer.de>
2026-03-24 13:47:23 +0100

    Created function for the method check. Compares all the methods of the location with the method of the request

---
+++ src/response/method_check.cpp
@@ -0,0 +1,11 @@
+#include "response.hpp"
+#include "LocationConfig.hpp"
+
+bool	checkMethod(const std::string &method, const LocationConfig location) {
+	for (const auto& s: location.methods) {
+		if (s == method)
+			return true;
+	}
+
+	return false;
+}
