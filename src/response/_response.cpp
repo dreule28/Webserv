9de84714bcc494f4c9985a8f729a95e6f86c7794
Anakin Pregitzer <anakin@pregitzer.de>
2026-03-25 11:38:20 +0100

    Embedded the methods in the response function

--- src/response/_response.cpp
+++ src/response/_response.cpp
@@ -13,17 +13,35 @@ std::string response(const http_request &request, const std::vector<LocationConf
 
 	std::string file_path(buildRealPath(loc));
 
-	if (checkFile(file_path, request.method) == -1) {
-		std::cerr << RED << "Error: " <<
-			"file does not exists or doesn't have the correct permissions"
-			<< RESET << std::endl;
-		return "";
+	if (request.method == "GET" || request.method == "DELETE") {
+		if (checkFile(file_path, request.method) == -1) {
+			std::cerr << RED << "Error: " <<
+				"file does not exists or doesn't have the correct permissions"
+				<< RESET << std::endl;
+			return "";
+		}
 	}
 
 	std::string res = "";
-	// TODO: execute method based on request.method
 
-	// res = GET()
+	if (request.method == "GET") {
+		res = GET(file_path);
+		if (res.empty()) {
+			std::cerr << RED << "Error: failed to read file" << RESET << std::endl;
+		}
+	}
+	else if (request.method == "POST") {
+		int status = POST(file_path, request.body);
+		std::stringstream ss;
+		ss << "HTTP/1.1 " << status << " " << HTTP_STATUS_MESSAGES.at(status) << "\r\n";
+		res = ss.str();
+	}
+	else if (request.method == "DELETE") {
+		int status = DELETE(file_path);
+		std::stringstream ss;
+		ss << "HTTP/1.1 " << status << " " << HTTP_STATUS_MESSAGES.at(status) << "\r\n";
+		res = ss.str();
+	}
 
 	return res;
 }
