851c5d06580f37565b54bab1000d77c9129804ac
Anakin Pregitzer <anakin@pregitzer.de>
2026-03-25 11:36:25 +0100

    Updated the function prototypes

--- inc/methods.hpp
+++ inc/methods.hpp
@@ -3,7 +3,12 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <filesystem>
 
-#define GET get_method
+#define GET		get_method
+#define POST	post_method
+#define DELETE	delete_method
 
-std::string	get_method(std::string &file_path);
+std::string		get_method(const std::string &file_path);
+int				post_method(const std::string &file_path, const std::string &content);
+int				delete_method(const std::string &file_path);
